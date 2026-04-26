ef40d10944cb53993a3df87e1c2d8cc9c502da65
harneetbagga <harneetbgg13@gmail.com>
2026-04-25 23:38:10 -0400

    updating env credetntials

--- ingestion/snowflake_connector.py
+++ ingestion/snowflake_connector.py
@@ -1,19 +1,18 @@
-#!/usr/bin/env python
+import os
+from dotenv import load_dotenv
 import snowflake.connector
 
+# Load environment variables
+load_dotenv()
+
 def snowflake_conn():
-    # Gets the version
     conn = snowflake.connector.connect(
-        user='harneetbgg13',
-        password='bUtkim=6bovpu=bajkip',
-        account='MRBPPUG-RY97098',
-        warehouse='COMPUTE_WH',
-        database='WALLMART_DB',
-        schema='GOLD',
-        role='ACCOUNTADMIN',
-        )
-
+        user=os.getenv("SNOWFLAKE_USER"),
+        password=os.getenv("SNOWFLAKE_PASSWORD"),
+        account=os.getenv("SNOWFLAKE_ACCOUNT"),
+        warehouse=os.getenv("SNOWFLAKE_WAREHOUSE"),
+        database=os.getenv("SNOWFLAKE_DATABASE"),
+        schema=os.getenv("SNOWFLAKE_SCHEMA"),
+        role=os.getenv("SNOWFLAKE.ROLE")
+    )
     return conn
-
-
-
